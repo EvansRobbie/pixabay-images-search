36fa66b562c42c2cbd6cd10d934f916c48771440
EvansRobbie <evansrobbie5311@gmail.com>
2024-03-07 18:44:59 +0300

    responsive image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -86,7 +86,7 @@ const Home = ({ searchTerm }: { searchTerm: string }) => {
   return (
     <>
       <h2 className="text-2xl font-bold mb-4">All images</h2>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1  md:grid-cols-3 gap-4">
         {isLoading ? (
           <p>Loading...</p>
         ) : images.length > 0 ? (
